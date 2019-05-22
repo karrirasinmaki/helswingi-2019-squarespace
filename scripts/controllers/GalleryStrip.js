4ac4a3d786e4583510b55906efab407541091854
Karri Rasinmäki <karri.rasinmaki@gmail.com>
2019-05-22 22:47:54 +0300

    Gallery slider scrollable on mobil

---
+++ scripts/controllers/GalleryStrip.js
@@ -0,0 +1,35 @@
+function GalleryStrip(element) {
+  const wrapper = element.querySelector('.sqs-wrapper');
+
+  const scrollPadding = -element.offsetLeft*2 + 40
+  let wrapperWidth = 0;
+  let lastScrollLeft = 0;
+
+  const enableScroll = () => {
+    element.style.overflow = 'scroll';
+  }
+
+  const updateWrapperWidth = () => {
+    let lastImg = wrapper.children[wrapper.children.length - 1];
+    wrapperWidth = lastImg.offsetLeft - lastImg.offsetWidth;
+  }
+
+  const onScroll = (evt) => {
+    updateWrapperWidth();
+    let maxScroll = wrapperWidth + wrapper.offsetLeft + scrollPadding;
+    if (element.scrollLeft > maxScroll) {
+      element.scrollLeft = maxScroll - (element.scrollLeft - lastScrollLeft);
+    }
+    lastScrollLeft = element.scrollLeft;
+  }
+
+  enableScroll();
+  element.addEventListener('scroll', onScroll);
+
+  return {
+    sync: enableScroll
+  };
+}
+
+
+export default GalleryStrip;
---
+++ scripts/custom.js
@@ -0,0 +1,11 @@
+import controller from '@squarespace/controller';
+
+import GalleryStrip from './controllers/GalleryStrip';
+
+controller.register('GalleryStrip', GalleryStrip);
+
+window.addEventListener('DOMContentLoaded', () => {
+  const galleryStrips = document.querySelectorAll('.sqs-gallery.sqs-gallery-design-strip')
+  Array.from(galleryStrips).forEach(element => element.setAttribute('data-controller', 'GalleryStrip'))
+  controller.refresh()
+})
